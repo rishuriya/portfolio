022b15d8fa10ce03686c1c40241873cf8e63e1aa
rishuriya <rdrishav2002@yahoo.com>
2022-12-13 02:33:12 +0530

    added base and sidebar

---
+++ components/Frontend/profile.tsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+    return (
+        <div className="w-[100vw] h-[100vh] bg-no-repeat bg-[url('../public/img/hero-bg.jpg')] bg-cover absolute">
+            <div className="flex flex-col justify-center items-center h-full">
+                <p className="font-bold text-6xl text-gray-100">Rishav Kumar</p>
+            
+                </div>
+            </div>
+
+    );
+};
